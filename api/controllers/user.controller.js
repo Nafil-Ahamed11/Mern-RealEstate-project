91c2a63bc58693a1d80ee2297f5797bfa3e927cd
Nafil-Ahamed11 <nafilahmeed123@gmail.com>
2024-06-14 01:51:15 +0530

    completed user update functioanlity

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,46 @@
+import User from "../models/user-model.js"
+import { errorHandler } from "../utils/error.js"
+import bcrypt from 'bcrypt';
 
 
 export const test = (req,res)=>{
     res.json({
         message:"Api testing.."
     })
+}
+
+export const updateUser = async (req, res, next) => {
+    console.log("Entered updateUser function");
+
+    if (req.user.id !== req.params.id) {
+        console.log("User ID mismatch");
+        return next(errorHandler(401, "You can only update your own account!"));
+    }
+
+    try {
+        if (req.body.password) {
+            console.log("Hashing password");
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+
+
+       
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        }, { new: true });
+
+        console.log("User updated", updatedUser);
+
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+
+    } catch (error) {
+        console.log("Error in updateUser:", error);
+        next(error);
+    }
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,15 +3,21 @@ import { useSelector } from 'react-redux';
 import { useRef } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
+import { updateUserStart,updateUserSuccess,updateUserFailure } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector(state => state.user);
+  const { currentUser,loading,error } = useSelector(state => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [uploadSuccess, setUploadSuccess] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+ 
+  console.log("current user ",currentUser);
 
   useEffect(() => {
     if (file) {
@@ -44,12 +50,45 @@ export default function Profile() {
     );
   };
 
+  const handleChange = (e) =>{
+    setFormData({...formData, [e.target.id]: e.target.value});
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    try {
+
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`,{
+
+        method:'POST',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if(data.success === false){
+        dispatch(updateUserFailure(data.message));
+        return
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+      
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
-        <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
-        <img onClick={() => fileRef.current.click()} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={formData.avatar || currentUser.avatar} alt="profile" />
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input onChange={(e) => setFile(e.target.files[0])} type="file"  ref={fileRef} hidden accept='image/*' />
+        <img onClick={() => fileRef.current.click()} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={formData.avatar || currentUser.avatar} onChange={handleChange} alt="profile" />
         <p className='text-sm self-center'>
           {fileUploadError ? (
             <span className='text-red-700'>Error Image Upload (image must be less then 2 mb)</span>
@@ -59,15 +98,17 @@ export default function Profile() {
             <span className='text-green-700'>Image successfully Uploaded!</span>
           ) : ""}
         </p>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-        <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+        <input type="text" placeholder='username' defaultValue={currentUser.username} className='border p-3 rounded-lg' onChange={handleChange} id='username' />
+        <input type="email" placeholder='email' defaultValue={currentUser.email} className='border p-3 rounded-lg'  onChange={handleChange} id='email' />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg'  onChange={handleChange} id='password' />
+        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...':'Update'}</button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess ? 'User update successfuly' : ''}</p>
     </div>
   );
 }
