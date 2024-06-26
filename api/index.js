2d7d3ebb18362119ac6a7dc1a678989289a66634
Nafil-Ahamed11 <nafilahmeed123@gmail.com>
2024-06-26 16:03:28 +0530

    added create lisitng api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import  dotenv  from 'dotenv';
-import userRouter from "./routes/user.route.js";
-import authRouter from  "./routes/auth.route.js";
+import userRouter from './routes/user.route.js';
+import authRouter from  './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js'
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -24,6 +25,8 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter);
+app.use('/api/listing',listingRouter);
+
 
 app.use((err,req,res,next)=>{
     const statuscode = err.statuscode || 500;
