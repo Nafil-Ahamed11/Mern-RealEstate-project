0d4e11697f2c62ca19eb801016c1e2294b384531
Nafil-Ahamed11 <nafilahmeed123@gmail.com>
2024-07-01 09:57:58 +0530

    completed create list page functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -5,8 +5,10 @@ export const createListing = async (req,res,next) => {
     console.log("entering inside of the listing function")
     try {
         const listing = await Listing.create(req.body);
+        console.log("body",req.body);
         return res.status(201).json(listing);
     } catch (error) {
+        console.error("Error creating listing:", error); 
         next(error);
     }
 }
